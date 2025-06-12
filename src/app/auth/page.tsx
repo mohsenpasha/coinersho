cee68922fa54d2bdc69bff22bf033823546b0ff1
unknown <omp13821380@gmail.com>
2025-06-12 10:13:00 +0330

    in useEffect we prevent input from being over 11 digit and also we have added submit btn to page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,15 +1,25 @@
 'use client'
 import { useEffect, useState } from "react";
 import PhoneInput from "../components/PhoneInput";
+import SubmiteButton from "../components/SubmitButton";
 
 export default function AuthPage(){
     const [inputValue,setInputValue] = useState('')
+    
     useEffect(()=>{
-        console.log(inputValue)
+        // this is for preventing more than 11 digit number
+        if(inputValue.length > 11){
+            setInputValue(inputValue.slice(0,11))
+        }
     },[inputValue])
+    
+    function submitHandler(){
+        console.log(inputValue)
+    }
     return(
         <div>
-            <PhoneInput value={inputValue} onchange={setInputValue}/>
+            <PhoneInput value={inputValue} changeHandler={setInputValue}/>
+            <SubmiteButton submitHandler={submitHandler}/>
         </div>
     )
 }
