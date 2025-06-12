e97a8d750f1118d35b43f50009a9d049cb0cbeb7
unknown <omp13821380@gmail.com>
2025-06-12 10:14:10 +0330

    a simple submit btn created

---
+++ src/app/components/SubmitButton.tsx
@@ -0,0 +1,9 @@
+export default function SubmiteButton({submitHandler}){
+    return(
+        <div>
+            <button onClick={submitHandler}>
+                ورود
+            </button>
+        </div>
+    )
+}
