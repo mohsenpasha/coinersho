575d77c841eb7dea4e6191f75427cce6d8048859
unknown <omp13821380@gmail.com>
2025-06-12 10:09:28 +0330

    now in input type number we prevent bad characters(e,+,-)

--- src/app/components/PhoneInput.tsx
+++ src/app/components/PhoneInput.tsx
@@ -1,10 +1,16 @@
-export default function PhoneInput({value,onchange}){
+import { Dispatch, KeyboardEvent, SetStateAction } from "react"
+
+export default function PhoneInput({value,changeHandler}:{value:string,changeHandler:Dispatch<SetStateAction<string>>}){
+    // this function is for preventing bad characters in input
+    function preventThings(event:KeyboardEvent<HTMLInputElement>){
+        if(event.key == 'e' || event.key == '+' || event.key == '-') event.preventDefault()
+    }
     return(
         <div>
             <span>
                 +98
             </span>
-            <input value={value} onChange={(event)=> onchange(event.target.value)} type="number" />
+            <input type="number" value={value} onKeyDown={(event)=>preventThings(event)} onChange={(event)=>changeHandler(event.target.value)} />
         </div>
     )
 }
