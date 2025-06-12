b11dc7bf3909608c7a821630dc69b5956679e7b7
unknown <omp13821380@gmail.com>
2025-06-12 14:05:34 +0330

    button disabled on loading

--- src/app/components/SubmitButton.tsx
+++ src/app/components/SubmitButton.tsx
@@ -2,7 +2,7 @@ import styles from './SubmitButton.module.scss'
 export default function SubmiteButton({submitHandler,isLoading}:{submitHandler:()=>void,isLoading:boolean}){
     return(
         <div>
-            <button className={styles.btn} onClick={submitHandler}>
+            <button disabled={isLoading} className={styles.btn} onClick={submitHandler}>
                 {isLoading
                 ?
                     <div className={styles.loading}></div>
